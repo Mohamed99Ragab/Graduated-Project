ba1afe8218c90675f298892c05bc1acfdb0505fd
Mohamed99Ragab <mohamedragabelgwile010@gmail.com>
2023-07-16 23:47:05 +0300

    edit route anlysis

--- routes/anlysis.php
+++ routes/anlysis.php
@@ -19,19 +19,23 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('users',[AnlysisController::class,'users']);
-Route::get('vaccines',[AnlysisController::class,'vaccinations']);
-Route::get('user-vaccines',[AnlysisController::class,'user_vaccinations']);
-Route::get('predictions',[AnlysisController::class,'ai_diseases']);
-Route::get('medical-details',[AnlysisController::class,'medical_datails']);
-Route::get('growth',[AnlysisController::class,'growth']);
 
-Route::get('teeth',[AnlysisController::class,'teeth']);
-Route::get('user-teeth',[AnlysisController::class,'user_teeth']);
 
-Route::get('medical-tests',[AnlysisController::class,'medical_tests']);
-Route::get('prescription',[AnlysisController::class,'prescription']);
 
+Route::group(['prefix'=>'elgwilepro1996'],function (){
+    Route::get('users',[AnlysisController::class,'users']);
+    Route::get('vaccines',[AnlysisController::class,'vaccinations']);
+    Route::get('user-vaccines',[AnlysisController::class,'user_vaccinations']);
+    Route::get('predictions',[AnlysisController::class,'ai_diseases']);
+    Route::get('medical-details',[AnlysisController::class,'medical_datails']);
+    Route::get('growth',[AnlysisController::class,'growth']);
+
+    Route::get('teeth',[AnlysisController::class,'teeth']);
+    Route::get('user-teeth',[AnlysisController::class,'user_teeth']);
+
+    Route::get('medical-tests',[AnlysisController::class,'medical_tests']);
+    Route::get('prescription',[AnlysisController::class,'prescription']);
+});
 
 
 
